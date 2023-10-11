062619be1d515a422bb0b1073ea5b6238f3cdf6e
ricki <sorrentino1818@gmail.com>
2023-10-11 13:09:01 +0200

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+    @me = "ciao a utti ragazzi io sononsore conme tustate tio tutto bene, i vi scrivo dall'home controller molto fi
+    figo per chimare o  fare api calls"
+  end
+end
